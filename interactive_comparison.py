9e83856fdbe9e703eb20f7422070152d86da91ab
Marc <marcubb@outlook.de>
2023-04-26 13:57:35 +0200

    added TODOs

---
+++ interactive_comparison.py
@@ -0,0 +1,26 @@
+# TODO: 4 interactive windows 1st: original, 2nd preprocessed, 3rd sobel, 4th canny, 5th XOR
+
+# TODO: original image:
+# change images with slider (more or less noise)
+
+# TODO: preprocessed image:
+# slider for sigma
+# slider for kernel_size_blur
+# slider for different preprocessing methods (n is dependant on kernel size)
+# slider for kernel size
+
+# TODO: sobel image:
+# slider for threshhold
+
+# TODO: canny image:
+# check opencv documentation for threshholds 
+# slider for low_threshhold
+# slider for high_threshhold
+# equalize threshholds
+
+# TODO: XOR image:
+# output for sum of pixels (error value)
+
+# TODO: try to minimize error value with sliders
+
+
